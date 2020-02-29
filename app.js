920db60ab36a6ebbd43f44342b35384ebbdc43d6
Max Rimmer Larsen <maxrimmer@me.com>
2020-02-29 19:51:23 +0000

    spelling log as log

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
 
-  console.out("WHAT THE FUCK ARE YOU DOING");
+  console.log("WHAT THE FUCK ARE YOU DOING");
   let body = req.body;
 
   // Checks this is an event from a page subscription
