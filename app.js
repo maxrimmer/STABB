ee85a0feda4a5fb82dbcd7a4252183cb5f286d2b
Max Rimmer Larsen <maxrimmer@me.com>
2020-03-01 02:30:48 +0000

    zoopla api key

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const ZOOPLA_API_TOKEN = process.env.ZOOPLA_API_TOKEN;
 const request = require('request');
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
