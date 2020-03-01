ee7eb3cc6f659973d9f1836c4a8f4c94539d3345
Max Rimmer Larsen <maxrimmer@me.com>
2020-03-01 01:43:37 +0000

    should be fixed

--- app.js
+++ app.js
@@ -26,9 +26,18 @@ app.post('/webhook', (req, res) => {
   let webhook_event = entry.messaging[0];
   console.log(webhook_event);
 
+
   // Get the sender PSID
   let sender_psid = webhook_event.sender.id;
   console.log('Sender PSID: ' + sender_psid);
+
+  // Check if the event is a message or postback and
+  // pass the event to the appropriate handler function
+  if (webhook_event.message) {
+    handleMessage(sender_psid, webhook_event.message);
+  } else if (webhook_event.postback) {
+    handlePostback(sender_psid, webhook_event.postback);
+  }
 
 });
 
